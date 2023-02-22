2da8629efab249b1d36dfeb19359b376385dbb62
jbdevelop3r <justinebanogon14@gmail.com>
2023-02-22 16:19:48 +0800

    deploy app

--- src/App.js
+++ src/App.js
@@ -4,19 +4,59 @@ import video from './video/video-gym.mp4'
 import { useInView } from "react-hook-inview";
 
 function App() {
-  const [offersRef, offersRefIsVisible] = useInView({
-    threshold: .3,
-    unobserveOnEnter: true
+  const [firstPageRef, firstPageRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [videoRef, videoRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [offerRef, offerRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [signupRef, signupRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  })
+  const [strengthRef, strengthRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [flexibilityhRef, flexibilityhRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [cardioRef, cardioRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [homeGymRef, homeGymRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [basicRef, basicRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [advancedRef, advancedRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
+  }) 
+  const [proRef, proRefIsVisible] = useInView({
+    threshold: .4,
+    unobserveOnEnter: false
   }) 
 
   return (
     <div className="main-container">
       <section className="first-page">
-          <div className="first-page-tagline">
+          <div className={`first-page-tagline ${firstPageRefIsVisible ? "show" : ""} `} ref={firstPageRef}>
             <p>One on one training</p>
             <p>Decide. Commit. Succeed. We will help you.</p>
           </div>
-          <div className="gym-video" >
+          <div className={`gym-video ${videoRefIsVisible ? "show" : ""} `} ref={videoRef} >
             <ReactPlayer
             width="100%"
             height="100%"
@@ -28,24 +68,24 @@ function App() {
           </div>
       </section>
       <section className="second-page">
-        <div className="exercise-tagline">
+        <div className={`exercise-tagline ${offerRefIsVisible ? "show" : ""} `} ref={offerRef}>
           <p> What makes our offers <code>stands</code> out from the rest? </p>
         </div>
 
-          <div  className={`four-exercises ${offersRefIsVisible ? "show" : ""} `} ref={offersRef}>
-            <div className="strength">
+          <div  className="four-exercises">
+            <div className={`strength ${strengthRefIsVisible ? "show" : ""} `} ref={strengthRef}>
               <p>strength</p>
               <p>power up your strength</p>
             </div>
-            <div className="flexibility">
+            <div className={`flexibility ${flexibilityhRefIsVisible ? "show" : ""} `} ref={flexibilityhRef}>
               <p>flexibility</p>
               <p>key to stability</p>
             </div>
-            <div className="cardio">
+            <div className={`cardio ${cardioRefIsVisible ? "show" : ""} `} ref={cardioRef}>
               <p>cardio</p>
               <p>secret to burn fats faster</p>
             </div>
-            <div className="home-gym">
+            <div className={`home-gym ${homeGymRefIsVisible ? "show" : ""} `} ref={homeGymRef}>
               <p>home gym</p>
               <p>work out even at home</p>
             </div>
@@ -56,7 +96,7 @@ function App() {
       <section className="third-page">
         <p>Try it for free</p>
         <p> We'll give you 2 weeks free training from one of our fitness trainer</p>
-        <button>Sign up now</button>
+        <button className={`signup-btn ${signupRefIsVisible ? "show" : ""} `} ref={signupRef}>Sign up now</button>
       </section>
 
       <section className="fourth-page">
@@ -64,19 +104,19 @@ function App() {
           <p>Plans for all level</p>
         </div>
         <div className="plans-container">
-          <div className="basic">
+          <div className={`basic ${basicRefIsVisible ? "show" : ""} `} ref={basicRef}>
             <div></div>
             <p>basic</p>
             <p>$5/month</p>
             <button>sign up</button>
           </div>
-          <div className="advanced">
+          <div className={`advanced ${advancedRefIsVisible ? "show" : ""} `} ref={advancedRef}>
             <div></div>
             <p>advanced</p>
             <p>$10/month</p>
             <button>sign up</button>
           </div>
-          <div className="pro">
+          <div className={`pro ${proRefIsVisible ? "show" : ""} `} ref={proRef}>
             <div></div>
             <p>pro</p>
             <p>$25/month</p>
